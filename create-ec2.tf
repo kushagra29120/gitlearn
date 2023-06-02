4950a43676f2ddf54f08a7b001b5d348486f3dbe
kushagra29120 <132610593+kushagra29120@users.noreply.github.com>
2023-06-03 02:39:57 +0530

    commit

---
+++ create-ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAZ5VB2JODX4S6WFFP"
+  secret_key = "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"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0889a44b331db0194"
+  instance_type = "t3.micro"
+}
