6d94e13608890275c5ee4591e1b9c1d2abf8c970
EC2 Default User <ec2-user@ip-172-31-92-88.ec2.internal>
2023-08-16 18:36:38 +0000

    testing

--- create-ec2.tf
+++ create-ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "BkdO2KYl86bmADzQH2bI9t/Apil0J+23PoZX1QpN"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web2" {
   ami           = "ami-0889a44b331db0194"
   instance_type = "t3.micro"
 }
